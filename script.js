65691a511f08481277df0c6802fb7934b13f274a
Nidhi Biswas <nidhibiswas7@gmail.com>
2025-11-09 12:16:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// <!-- typed js effect starts -->
+
+var typed = new Typed(".typing-text", {
+    strings: ["", "Software Development", "Web Designing", "Full Stack Development", "Data Science", "CyberSecurity"],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 85,
+    backDelay: 500,
+});
+
